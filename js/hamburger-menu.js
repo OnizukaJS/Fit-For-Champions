98b3aee507574d1b1a127d884506d992792f343b
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-07-17 14:52:56 +0200

    changed the hamburger menu - FINAL ONE

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -4,9 +4,17 @@ hamburgerIcon.addEventListener('click', function () {
     let menuResp = document.getElementById('menuResp');
     if (menuResp.style.display === 'none') {
         menuResp.style.display = 'flex';
+        hamburgerIcon.style.backgroundColor = '#0C1B33';
+        hamburgerIcon.style.border = '1px solid';
+        hamburgerIcon.style.color = 'white';
+        hamburgerIcon.style.outline = 'none';
         menuResp.classList.add('display-menu-resp');
     } else {
         menuResp.style.display = 'none';
         menuResp.classList.remove('display-menu-resp');
+        hamburgerIcon.style.backgroundColor = 'white';
+        hamburgerIcon.style.border = 'none';
+        hamburgerIcon.style.color = '#0C1B33';
+        hamburgerIcon.style.outline = 'none';
     }
 });
